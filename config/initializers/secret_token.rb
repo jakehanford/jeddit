072adfd744703b77dd0295ab1a0863ca569a022c
Jack Hanford <jackhanford@gmail.com>
2013-09-16 12:55:30 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeddit::Application.config.secret_token = 'ebd800fb117d402ec15872a4a25ca9cc7cd5a3790dee53637c9348e18bca37b0f6ac35a42e30db04399fd35f82cca07ce36c9e86bc8efd9924092d346defc896'

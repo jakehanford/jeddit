03d774f38df64162d790cdb9df44e4201a8a9765
Jackhanford <jackhanford@gmail.com>
2013-10-02 18:36:53 -0700

    fixed odd error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  attr_accessible :body, :user_id
+  belongs_to :user
+  attr_accessible :body, :post, :user_id
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true

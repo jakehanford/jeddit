b1cb1cff9b99ae2dc8041d89660df0df255d6949
Jackhanford <jackhanford@gmail.com>
2013-10-22 15:33:51 -0700

    Small touches here'n there

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".addComment").click(function(e) {
   	e.preventDefault();
-    $(".commentForm.none").slideDown();
+    $(".commentForm.none").fadeIn();
     $(this).hide();
   });
 });
